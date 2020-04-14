ba09fc34da5a58d63e3faa4d9d10782624a0970f
akshay_magrani <61657232+akshaymagrani@users.noreply.github.com>
2020-04-14 23:19:23 +0530

    Design match challenge - Project tracking

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function disappear() {
+    var iconDisappear = document.getElementById("icon-hamburger");
+    var divNav = document.getElementById("div-nav");
+    var iconAppear = document.getElementById("icon-close");
+    iconDisappear.style.display = "none";
+    divNav.style.display = "block";
+    iconAppear.style.display = "block";
+}
